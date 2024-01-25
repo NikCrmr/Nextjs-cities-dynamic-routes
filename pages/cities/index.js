12170407a9f23d4f227fd5c6f488bed8c62f0bdd
Niko Cremer <n.ko@posteo.de>
2024-01-25 10:19:16 +0100

    intitial commit + add cities component + data.js object

---
+++ pages/cities/index.js
@@ -0,0 +1,6 @@
+import { cities } from "@/lib/data";
+
+export default function Cities(cities) {
+  return;
+  cities.map(city);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Cities from "./cities/index";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Hello from Niko 😀</h1>
+      <Cities />
     </div>
   );
 }
